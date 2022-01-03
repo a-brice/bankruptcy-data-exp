115b41142d47f4393e8781937955e95d8b85d531
Brice <akouvibrice@gmail.com>
2022-01-03 03:33:16 +0100

    API Templates

--- api/api/settings.py
+++ api/api/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
@@ -121,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- api/core/views.py
+++ api/core/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import redirect, render, HttpResponse
+from django.core.files.storage import FileSystemStorage
+import pandas as pd
+from django.conf import settings
 # Create your views here.
 
 def index(req):
     return render(req, 'core/index.html')
+
+def viz(req):
+    return render(req, 'core/viz.html')
+
+
+def predict(req):
+    context = {}
+    if req.POST:
+        year = req.POST['year']
+        if req.POST['options_input1'] == "csv":
+            file = req.FILES['file']
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            context['prediction'] = model_prediction(file=filename, year=year)
+        if req.POST['options_input1'] == "entry":
+            entry = req.POST['entry']
+            context['prediction'] = model_prediction(entry=entry, year=year)
+
+    return render(req, 'core/index.html', context=context)
+
+
+def model_prediction(file=None, entry=None, year=None):
+    if file:
+        data = pd.read_csv('.' + settings.MEDIA_URL + file)
+        data['year'] = year
+        data = data.head(10)
+    elif entry:
+        data = pd.DataFrame([year] + entry.split(',')).T
+        data.columns = ['year'] + [f'X{i}' for i in range(1,65)]
+
+    return data.to_html()
